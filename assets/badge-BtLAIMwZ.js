0fb2bcee1033517aa8aa0827beea1388242df6a7
Jayadev Vasudevan <jayadevmarath@gmail.com>
2025-09-20 17:34:45 +0530

    Updates

---
+++ assets/badge-BtLAIMwZ.js
@@ -0,0 +1,11 @@
+var g=Object.defineProperty;var o=Object.getOwnPropertySymbols;var i=Object.prototype.hasOwnProperty,c=Object.prototype.propertyIsEnumerable;var n=(r,t,e)=>t in r?g(r,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):r[t]=e,d=(r,t)=>{for(var e in t||(t={}))i.call(t,e)&&n(r,e,t[e]);if(o)for(var e of o(t))c.call(t,e)&&n(r,e,t[e]);return r};var u=(r,t)=>{var e={};for(var a in r)i.call(r,a)&&t.indexOf(a)<0&&(e[a]=r[a]);if(r!=null&&o)for(var a of o(r))t.indexOf(a)<0&&c.call(r,a)&&(e[a]=r[a]);return e};import{c as f,f as l,g as p}from"./index-CKkHQYAn.js";import{j as v}from"./ui-CdYL37x2.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=f("Heart",[["path",{d:"M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z",key:"c3ymky"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const M=f("MessageCircle",[["path",{d:"M7.9 20A9 9 0 1 0 4 16.1L2 22Z",key:"vv11sd"}]]),b=p("inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",{variants:{variant:{default:"border-transparent bg-primary text-primary-foreground hover:bg-primary/80",secondary:"border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",destructive:"border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",outline:"text-foreground"}},defaultVariants:{variant:"default"}});function j(a){var s=a,{className:r,variant:t}=s,e=u(s,["className","variant"]);return v.jsx("div",d({className:l(b({variant:t}),r)},e))}export{j as B,h as H,M};
