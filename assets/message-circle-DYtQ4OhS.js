fd0ef6bca66448cee94929d55385c2ec5ee9fda9
Jayadev Vasudevan <jayadevmarath@gmail.com>
2026-03-01 16:35:35 +0530

    Updates

--- assets/message-circle-DYtQ4OhS.js
+++ assets/message-circle-DYtQ4OhS.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-79hseCbg.js";/**
+import{c as e}from"./index-v972zg3r.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
