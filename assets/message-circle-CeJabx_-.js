b9696042fd112a6bc744a9ad48105473ed59de51
Jayadev Vasudevan <jayadevmarath@gmail.com>
2026-03-01 17:00:41 +0530

    Updates

--- assets/message-circle-CeJabx_-.js
+++ assets/message-circle-CeJabx_-.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-v972zg3r.js";/**
+import{c as e}from"./index-Di0UNhT_.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
